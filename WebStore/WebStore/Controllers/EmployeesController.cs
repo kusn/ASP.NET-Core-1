f300161420d4c0c7dcdea44d4b243c7c61f4ce08
Kusn <serjikf@yandex.ru>
2021-09-19 14:02:42 +0300

    Добавлен класс TestData с сотрудниками

--- WebStore/WebStore/Controllers/EmployeesController.cs
+++ WebStore/WebStore/Controllers/EmployeesController.cs
@@ -4,21 +4,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Models;
+using WebStore.Data;
 
 namespace WebStore.Controllers
 {
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> __Employees = new()
+        private readonly IEnumerable<Employee> _Employees;
+
+        public EmployeesController()
         {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27, DateOfBorn = Convert.ToDateTime("01.01.2001"), DateOfEmployment = Convert.ToDateTime("02.02.2019") },
-            new Employee { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 31, DateOfBorn = Convert.ToDateTime("03.03.2002"), DateOfEmployment = Convert.ToDateTime("04.04.2020") },
-            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18, DateOfBorn = Convert.ToDateTime("05.05.2003"), DateOfEmployment = Convert.ToDateTime("06.06.2021") }
-        };
+            _Employees = TestData.Employees;
+        }
 
         public IActionResult Index()
         {
-            return View(__Employees);
+            return View(_Employees);
         }
 
         /*public IActionResult Employees()                // http://localhost:5000/Home/Employees
---
+++ WebStore/WebStore/Data/TestData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Data
+{
+    public static class TestData
+    {
+        public static List<Employee> Employees { get; set; } = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27, DateOfBorn = Convert.ToDateTime("01.01.2001"), DateOfEmployment = Convert.ToDateTime("02.02.2019") },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 31, DateOfBorn = Convert.ToDateTime("03.03.2002"), DateOfEmployment = Convert.ToDateTime("04.04.2020") },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18, DateOfBorn = Convert.ToDateTime("05.05.2003"), DateOfEmployment = Convert.ToDateTime("06.06.2021") }
+        };
+    }
+}
