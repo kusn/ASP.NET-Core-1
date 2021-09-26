96892c576feddde0cb97e78942f4c09aace2714a
Kusn <serjikf@yandex.ru>
2021-09-26 09:35:18 +0300

    Реализация сервиса каталога товаров

--- WebStore/WebStore/Startup.cs
+++ WebStore/WebStore/Startup.cs
@@ -23,6 +23,7 @@ namespace WebStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IEmployeesData, InMemoryEmployeesData>();
+            services.AddSingleton<IProductData, InMemoryProductData>();
 
             services.AddControllersWithViews(opt => opt.Conventions.Add(new TestControllerConvention()))
                 .AddRazorRuntimeCompilation();
