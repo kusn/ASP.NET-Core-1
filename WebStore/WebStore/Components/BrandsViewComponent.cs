6de8e8e18fd14c6bf359bda1d57841ba07bfcccc
Kusn <serjikf@yandex.ru>
2021-09-26 09:58:01 +0300

    Заготовки визуальных компонент брендов и продуктов

---
+++ WebStore/WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Components
+{
+    //[ViewComponent(Name = "BrandsView")]
+    public class BrandsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+        /*public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }*/
+    }
+}
