0eac5d85ebefde8ef5c9721ab36af08cfe5d4db3
Kusn <serjikf@yandex.ru>
2021-09-26 09:31:45 +0300

    Сервис каталога товаров + набор тестовых данных

---
+++ WebStore/WebStore/Services/InMemoryProductData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Data;
+using WebStore.Domain.Entities;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Services
+{
+    public class InMemoryProductData : IProductData
+    {
+        public IEnumerable<Brand> GetBrands()
+        {
+            return TestData.Brands;
+        }
+
+        public IEnumerable<Section> GetSections()
+        {
+            return TestData.Sections;
+        }
+    }
+}
