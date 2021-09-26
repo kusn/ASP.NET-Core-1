9ad339a6a413ec1d461d74dc9c732e991fbf3bbb
Kusn <serjikf@yandex.ru>
2021-09-26 10:54:29 +0300

    Логика визуального компонента секций

--- WebStore/WebStore/Components/SectionsViewComponent.cs
+++ WebStore/WebStore/Components/SectionsViewComponent.cs
@@ -3,14 +3,57 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Components
 {
     public class SectionsViewComponent : ViewComponent
     {
+        public IProductData _ProductData { get; }
+
+        SectionsViewComponent(IProductData ProductData)
+        {
+            _ProductData = ProductData;
+        }        
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var sections = _ProductData.GetSections();
+
+            var parentSections = sections.Where(s => s.ParentId is null);
+
+            var parentSectionsViews = parentSections
+                .Select(s => new SectionViewModel
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    Order = s.Order,
+                }).ToList();
+
+            foreach(var parentSection in parentSectionsViews)
+            {
+                var childs = sections.Where(s => s.Id == parentSection.Id);
+
+                foreach (var childSections in childs)
+                {
+                    parentSection.ChildSections.Add(new SectionViewModel
+                    {
+                        Id = childSections.Id,
+                        Name = childSections.Name,
+                        Order = childSections.Order,
+                        Parent = parentSection,
+                    });
+                }
+
+                parentSection.ChildSections.Sort((a, b) => 
+                    Comparer<int>.Default.Compare(a.Order, b.Order));
+            }
+
+            parentSectionsViews.Sort((a, b) =>
+                    Comparer<int>.Default.Compare(a.Order, b.Order));
+
+            return View(parentSectionsViews);
         }
     }
 }
