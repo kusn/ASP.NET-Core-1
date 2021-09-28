c28d02136083f2d3b2ae53cf7f69da8bd627f74e
Kusn <serjikf@yandex.ru>
2021-09-28 20:06:38 +0300

    Рефакторинг сервисов

--- WebStore/WebStore/Startup.cs
+++ WebStore/WebStore/Startup.cs
@@ -9,7 +9,7 @@ using WebStore.DAL.Context;
 using WebStore.Data;
 using WebStore.Infrastructure.Conventions;
 using WebStore.Infrastructure.Middleware;
-using WebStore.Services;
+using WebStore.Services.InMemory;
 using WebStore.Services.Interfaces;
 
 namespace WebStore
