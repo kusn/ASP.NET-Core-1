4c397f11f5c956f4ae2ef95799798b1c53d67eff
Kusn <serjikf@yandex.ru>
2021-09-28 12:50:31 +0300

    Доработка набора сущностей для использования их EF

--- WebStore/WebStore.Domain/Entities/Product.cs
+++ WebStore/WebStore.Domain/Entities/Product.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +10,24 @@ using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities
 {
+    [Index(nameof(Name)]
     public class Product : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
 
         public int SectionId { get; set; }
 
+        [ForeignKey(nameof(SectionId))]
+        public Section Section { get; set; }
+
         public int? BrandId { get; set; }
+
+        [ForeignKey(nameof(BrandId))]
+        public Brand Brand { get; set; }
 
         public string ImageUrl { get; set; }
 
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
