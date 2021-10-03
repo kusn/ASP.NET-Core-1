b874171fc17bda6f66b64a56d8f07255f64be7c7
Kusn <serjikf@yandex.ru>
2021-10-03 10:58:07 +0300

    Добавление менеджеров Identity

--- WebStore/WebStore/Controllers/AccountController.cs
+++ WebStore/WebStore/Controllers/AccountController.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain.Identity;
 
 namespace WebStore.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> _UserManager;
+        private readonly SignInManager<User> _SignInManager;
+
+        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
+        {
+            _UserManager = UserManager;
+            _SignInManager = SignInManager;
+        }
+
         public IActionResult Register()
         {
             return View();
@@ -18,10 +29,8 @@ namespace WebStore.Controllers
             return View();
         }
 
-        public IActionResult Logout()
-        {
-            RedirectToAction("Index", "Home");
-        }
+        public IActionResult Logout() => RedirectToAction("Index", "Home");
+
 
         public IActionResult AccsessDenied()
         {
