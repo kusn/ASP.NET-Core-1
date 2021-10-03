d4e06e211745320feb4321f357d6eff6964f5973
Kusn <serjikf@yandex.ru>
2021-10-03 10:52:39 +0300

    Заготовка контроллера Account

---
+++ WebStore/WebStore/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult AccsessDenied()
+        {
+            return View();
+        }
+    }
+}
