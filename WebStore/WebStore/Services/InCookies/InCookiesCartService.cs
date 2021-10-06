300698bd9efe6e03e1ee241c783550f61b4e3cc2
Kusn <serjikf@yandex.ru>
2021-10-06 20:42:18 +0300

    Заготовка сервиса корзины

---
+++ WebStore/WebStore/Services/InCookies/InCookiesCartService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Services.InCookies
+{
+    public class InCookiesCartService : ICartService
+    {
+        private readonly IHttpContextAccessor _HttpContextAccessor;
+        private readonly IProductData _ProductData;
+        private readonly string _CartName;
+
+        public InCookiesCartService(IHttpContextAccessor HttpContextAccessor, IProductData ProductData)
+        {
+            _HttpContextAccessor = HttpContextAccessor;
+            _ProductData = ProductData;
+
+            var user = HttpContextAccessor.HttpContext!.User;
+            var user_name = user.Identity!.IsAuthenticated ? $"{user.Identity.Name}" : null;
+
+            _CartName = $"GB.WebStore.Cart{user_name}";
+        }
+        
+        public void Add(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Decrement(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CartViewModel GetViewModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
