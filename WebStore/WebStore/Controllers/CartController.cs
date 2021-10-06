7f397ea1a508fe4551db96c422b17fd0ea508b38
Kusn <serjikf@yandex.ru>
2021-10-06 22:16:55 +0300

    Логика контроллера и сервиса корзины

--- WebStore/WebStore/Controllers/CartController.cs
+++ WebStore/WebStore/Controllers/CartController.cs
@@ -3,14 +3,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Services.Interfaces;
 
 namespace WebStore.Controllers
 {
     public class CartController : Controller
     {
+        private readonly ICartService _CartService;
+
+        public CartController(ICartService CartService)
+        {
+            _CartService = CartService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_CartService.GetViewModel());
+        }
+
+        public IActionResult Add(int id)
+        {
+            _CartService.Add(id);
+            return RedirectToAction("Index", "Cart");
+        }
+
+        public IActionResult Decrement(int id)
+        {
+            _CartService.Decrement(id);
+            return RedirectToAction("Index", "Cart");
+        }
+
+        public IActionResult Remove(int id)
+        {
+            _CartService.Remove(id);
+            return RedirectToAction("Index", "Cart");
         }
     }
 }
